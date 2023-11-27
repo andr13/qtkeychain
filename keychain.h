559c816716bf4a988b9c60a9b44d07704c70d9eb
Andrey Zaharow <andr290606@gmail.com>
2023-11-27 16:51:31 +0200

    Qt 6 build configuration

--- keychain.h
+++ keychain.h
@@ -9,11 +9,11 @@
 #ifndef KEYCHAIN_H
 #define KEYCHAIN_H
 
-#if !defined(QTKEYCHAIN_NO_EXPORT)
-#include "qkeychain_export.h"
-#else
+//#if !defined(QTKEYCHAIN_NO_EXPORT)
+//#include "qkeychain_export.h"
+//#else
 #define QKEYCHAIN_EXPORT
-#endif
+//#endif
 
 #include <QtCore/QObject>
 #include <QtCore/QString>
